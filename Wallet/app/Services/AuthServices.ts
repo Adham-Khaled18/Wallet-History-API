781cee0d689ba152b528047471f2beb8da9e9815
BunnyZ18 <adham@mrkhalednasr.com>
2022-10-12 05:51:33 +0200

    git commit

--- Wallet/app/Services/AuthServices.ts
+++ Wallet/app/Services/AuthServices.ts
@@ -3,6 +3,7 @@ import Mail from '@ioc:Adonis/Addons/Mail'
 import Env from '@ioc:Adonis/Core/Env'
 import { DateTime } from "luxon"
 import Route from '@ioc:Adonis/Core/Route'
+import Application from '@ioc:Adonis/Core/Application'
 
 
 
@@ -15,22 +16,25 @@ class AuthService {
             .firstOrFail()
     }
 
-    public static async SendEmail(user){
-        
-        const url = Env.get('APP_URL')+ Route.makeSignedUrl('verifyEmail',{params:{email:user.email},expiresIn:'30m'})
+    public static async SendEmail(user) {
+
+        const url = Env.get('APP_URL') + Route.makeSignedUrl('verifyEmail', { params: { email: user.email }, expiresIn: '30m' })
         Mail.send((message) => {
             message
-              .from('adham@mrkhalednasr.com')
-              .to(user.email)
-              .subject('Verify Email')
-              .htmlView('emails/verify', { user ,url})
-          })
+                .embed(Application.resourcesPath('images/image-1.png'),
+                'sora')
+                .from('adham@mrkhalednasr.com')
+                .to(user.email)
+                .subject('Verify Email')
+                .htmlView('emails/verify', { user, url })
+
+        })
     }
 
-    public static async VerifyEmail(email){
-        const user = await User.findByOrFail('email',email)
-            user.email_verified_at = DateTime.local()
-            user.save()
+    public static async VerifyEmail(email) {
+        const user = await User.findByOrFail('email', email)
+        user.email_verified_at = DateTime.local()
+        user.save()
     }
 }
 export default AuthService
